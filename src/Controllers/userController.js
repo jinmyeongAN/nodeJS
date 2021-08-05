85c973d29a3b5f8610aeffd33842b78e98f38072
jinmyeongAN <jinmyeong.an@gmail.com>
2021-08-05 15:49:17 +0900

    pug mixin practice

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,4 +1,20 @@
 export const handleEdit = (req, res) => {
-    const videos = ['a','b','c','d','e'];
+    const videos = [
+        {
+            title: "movie_1",
+            views: 54,
+            rating: 3
+        },
+        {
+            title: "movie_2",
+            views: 84,
+            rating: 7
+        },
+        {
+            title: "movie_3",
+            views: 9,
+            rating: 6
+        }
+    ]
     res.render("home", {pageName:"home", videos}); 
 }
